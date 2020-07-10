a67849f37590307b473f1121074f08eb1e760b7c
SourFruit   <65507526+hack-everything@users.noreply.github.com>
2020-07-09 20:56:51 -0400

    Possible Final

---
+++ web-server.py
@@ -0,0 +1,64 @@
+
+import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from tqdm import tqdm
+import time
+
+loop = tqdm(total = 100, position=0, leave=False)
+for k in range(100):
+    loop.set_description("Creating Your Web Server".format(k))
+    loop.update(1)
+    time.sleep(0.05)
+
+
+print(" ")
+
+yesChoice = ['yes', 'y']
+noChoice = ['no', 'n']
+
+input = input('Would you like the link to your web server? >_ ').lower()
+if input in yesChoice:
+    loop = tqdm(total=100, position=0, leave=False)
+    for k in range(100):
+        loop.set_description("Generating Your Link".format(k))
+        loop.update(1)
+        time.sleep(0.05)
+    print(" ")
+    print('Access The Web Server Here:')
+    print('http://127.0.0.1:4200/server.html')
+    time.sleep(3)
+
+    loop = tqdm(total=100, position=0, leave=False)
+    for k in range(100):
+        loop.set_description("Generating Your Link".format(k))
+        loop.update(1)
+        time.sleep(0.05)
+
+
+    print(" ")
+    print('Access The Web Server Index Here:')
+    print('http://127.0.0.1:4200')
+
+elif input in noChoice:
+    print('Okay, Thanks For Using Web Server Creator!_')
+
+else:
+    exit('I\'m sorry, You entered the wrong choice(s). The choices were: "yes" , "y" , "no" and "n". Please re-run Web Server Creator')
+
+class Serv(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "Listen, did you really just try to open a non-existent file, you idiot?"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+httpd = HTTPServer(('127.0.0.1', 4200), Serv)
+httpd.serve_forever()
