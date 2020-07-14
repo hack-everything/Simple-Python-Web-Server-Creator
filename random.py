6d82c9db6588f284408e909138be438e588f5cc0
SourFruit   <65507526+hack-everything@users.noreply.github.com>
2020-07-13 23:51:16 -0400

    Update random.py

--- random.py
+++ random.py
@@ -1,1 +1,1 @@
-# You don't need this but GH claims this repo as "HTML" yet its python so I'm adding more BS to have a Python marker
+print('You don't need this but GH claims this repo as "HTML" yet its python so I'm adding more BS to have a Python marker')
